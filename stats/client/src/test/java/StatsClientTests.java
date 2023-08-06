cd8be88eb844b038537d2c5e181e6626b6b2e0ca
nikitatuseev <tuseevn@mail.ru>
2023-08-07 00:54:44 +0300

    statistics service

--- stats/client/src/test/java/StatsClientTests.java
+++ stats/client/src/test/java/StatsClientTests.java
@@ -21,7 +21,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class StatsClientTest {
+class StatsClientTests {
 
     @Mock
     private RestTemplate restTemplate;

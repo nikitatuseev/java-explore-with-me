aee847ddce92f453853b6a89e0f34fc4ca5256a2
nikitatuseev <tuseevn@mail.ru>
2023-08-18 23:28:37 +0300

    main-service

--- stats/server/src/main/java/practicum/stat/StatsService.java
+++ stats/server/src/main/java/practicum/stat/StatsService.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 public interface StatsService {
     void sendHit(HitDto hitDto);
+
     List<UriStatDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
 }

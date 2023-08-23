d72f2ae554a3609358af88d15116425875136425
nikitatuseev <tuseevn@mail.ru>
2023-08-24 02:34:51 +0300

    update README.md

--- ewm-service/src/main/java/ru/practicum/exception/CommentException.java
+++ ewm-service/src/main/java/ru/practicum/exception/CommentException.java
@@ -1,6 +1,6 @@
 package ru.practicum.exception;
 
-public class CommentException extends RuntimeException{
+public class CommentException extends RuntimeException {
     public CommentException(String s) {
         super();
     }
